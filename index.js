e534a616e2a117f28c73717ad8a059af4a7625aa
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-06-08 21:16:32 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = function () {
+    return 2000 / 100;
+}
+
+const square = num1 => num1**2;
+
+console.log(square(4));
+
+const add = (num1, num2) => num1 + num2;
+
+console.log (add(12,45));
